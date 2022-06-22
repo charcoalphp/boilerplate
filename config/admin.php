08dd168609d2cf5a6cd808a5dbcf91878b7e82b7
Joel Alphonso <joel@locomotive.ca>
2022-06-22 18:28:33 -0400

    refactor: :recycle: upgrade boilerplate for charcoal/charcoal

--- config/admin.php
+++ config/admin.php
@@ -7,4 +7,4 @@ $this->addFile(__DIR__ . '/elfinder.json');
 $this->addFile(__DIR__ . '/admin.json');
 
 /** Import `charcoal-attachment` routes */
-$this->addFile(dirname(__DIR__) . '/vendor/locomotivemtl/charcoal-attachment/config/admin.json');
+$this->addFile(dirname(__DIR__) . '/%packages.path%/attachment/config/admin.json');
